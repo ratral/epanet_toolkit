a073903643ba84e41f42c095d38041952069dc8a
Dr. Raul Trujillo <dr.ing.trujillo@gmail.com>
2018-08-02 16:57:23 +0200

    Function for the Row based summary calculations

--- func/epanet_api_functions.R
+++ func/epanet_api_functions.R
@@ -3,6 +3,22 @@
 # Plot Time Series
 #...............................................................................
 
+Stats_calc <- function(x){
+  min    <- min(x, na.rm=TRUE)
+  q25    <- quantile(x, probs=0.25, na.rm=TRUE )
+  q50    <- quantile(x, probs=0.50, na.rm=TRUE )
+  mean   <- round(mean(x, na.rm=TRUE),2)
+  q75    <- quantile(x, probs=0.75, na.rm=TRUE )
+  max    <- max(x, na.rm=TRUE)
+  sd     <- round(sd(x, na.rm=TRUE),2)
+  dvalue <- max-min
+  return(c(Min=min, Qu=q25, Qu=q50, Mean=mean, Qu=q75, Max=max, SD=sd, D.Value=dvalue))
+}
+
+#...............................................................................
+# Plot Time Series
+#...............................................................................
+
 plot_ts_curves <- function (ts_curve,
                             m_title = "",
                             y_limits = c(0,7),
@@ -12,18 +28,60 @@ plot_ts_curves <- function (ts_curve,
                             time_minor  = "30 min",
                             x_labtls    = "%H:%M") {
   
-  x_limits <- c(ymd_hm("2020-1-1 0:00"), ymd_hm("2020-1-1 24:00"))
-  
   autoplot.zoo(ts_curve, facets = NULL) +
-  ggtitle(m_title)+
-  xlab(x_lab) + 
-  ylab(y_lab) +
+  ggtitle(m_title)+ xlab(x_lab) + ylab(y_lab) +
   scale_x_datetime(breaks       = date_breaks(time_breaks), 
                    minor_breaks = date_breaks(time_minor), 
-                   labels       = date_format(x_labtls),
-                   limits       = x_limits) +
+                   labels       = date_format(x_labtls)) +
   ylim(y_limits)+
   theme_hc() + scale_colour_hc()
 }
-#...............................................................................
 
+#...............................................................................
+# select report of the netdata
+# report: report name
+# result: nodes or links
+# Type: 
+#    For Links: Pipe, Pump, PRV,  PSV,PBV, FCV
+#    For Nodes: Junction, Tank, Reservoir
+# value:
+#    For Links: Flow Velocity Headloss
+#    For Nodes: Elevetion, Demand, Head, Pressure  
+# id: RegExp of the nodes or links 
+# 
+# serie_index
+
+
+tab_reports <- function(report,results, type, id, value, summary = FALSE){
+  
+  if(results == "nodes"){
+    tab_results <- as.tibble(report$nodeResults) %>%
+      subset(nodeType == type & grepl(id,ID )) 
+  }
+  
+  if (results == "links"){
+    tab_results <- as.tibble(report$linkResults) %>%
+      subset(linkType == type & grepl(id,ID )) 
+  }
+  
+  tab_results <-tab_results %>%
+    select(timeInSeconds, ID, value) %>%
+    spread(ID, value) 
+  
+  if (summary == TRUE){
+    tab_results <- cbind(tab_results, 
+                         t(apply(tab_results[,2:dim(tab_results)[2]],1, Stats_calc)))
+  }
+  
+  idx <- today() + seconds(tab_results$timeInSeconds)
+  
+  tab_results <- tab_results %>%
+                 select(-timeInSeconds) %>%
+                 as.zoo(idx)
+}
+
+
+#<- tab_results %>%
+#  subset(grepl(id,ID ))
+#select(ID, timeInSeconds, value) %>%
+#  spread(ID, value)
