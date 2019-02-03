d72116acf5e2e29e2b2e89892b9d343ccd132db9
Dr. Raul Trujillo Alvarez <dr.ing.trujillo@gmail.com>
2019-02-03 10:26:23 +0100

    Update SensitivityMatrix.R

--- SensitivityMatrix.R
+++ SensitivityMatrix.R
@@ -1,160 +1,68 @@
 #...............................................................................
-# 1. - Initialize Session                                                  #### 
+# 1. - Initialize Session 
 #...............................................................................
 
 cat("\014")
 rm(list=ls()) 
 
-# Installs libraries 
+#...............................................................................
+# 2. - Installs libraries 
+#...............................................................................
+
 library(tidyverse)
-library(dplyr)
-library(zoo)
-library(lubridate)
-library(epanetReader)
-library(epanet2toolkit)
-library(ggfortify)
-library(ggthemes)
-library(scales)
-library(purrr)
-library(visNetwork)
-
-# Initialize params
-params <- list(base_network    = "base_dma_02", 
-               new_network     = "base_dma_w_leaks",
-               functs_name     = "epanet_api_functions",
-               inlet_valves    = "PRV_",
-               jt_to_analyze   = "^JT_0[A-K]", # RegExp
-               pipe_to_analyze = "PS_",        # RegExp
-               leak_rate       = 0.01, # Percentage of the network with leaks
-               demad_factor    = list( names =c( "wd_spring_summer",
-                                                 "hw_spring_summer",
-                                                 "wd_summer_break",
-                                                 "hw_summer_break",
-                                                 "wd_fall_winter",
-                                                 "hw_fall_winter"),
-                                       factors = c( 0.92, 1.00, 1.09, 
-                                                    0.81, 0.66, 0.95)),
-               work_folders   = list( dir_work   = getwd(),
-                                      dir_report = file.path(getwd(),"reports"),
-                                      dir_data   = file.path(getwd(),"data"),  
-                                      dir_bin    = file.path(getwd(),"reports"),
-                                      dir_func   = file.path(getwd(),"func")))
-
-params$f_names <- list( base_file_inp    = file.path(params$work_folders$dir_data, 
-                                                     paste0(params$base_network,".inp")),
-                        base_file_report = file.path(params$work_folders$dir_report, 
-                                                     paste0(params$base_network,".rpt")),
-                        new_file_inp     = file.path(params$work_folders$dir_data, 
-                                                     paste0(params$new_network,".inp")),
-                        new_file_report  = file.path(params$work_folders$dir_report,
-                                                     paste0(params$new_network,".rpt")),
-                        file_func        = file.path(params$work_folders$dir_func, 
-                                                     paste0(params$functs_name,".R")))
-
-
-# Load Functions Standard
-source(params$f_names$file_func)
 
 #...............................................................................
-# 2. GENERATE THE LEAKAGE IN A NEW NETWORK                                  ####
+# 3, - Restore the DB of the calculation 
 #...............................................................................
 
-# Read network information from an *.inp
-
-net_input_01  <- read.inp(params$f_names$base_file_inp)
-
-net_input_01 <- gen_network_w_leaks(net_input_01, 
-                                    params$leak_rate, 
-                                    params$jt_to_analyze )
-
-
-write.inp(net_input_01, params$f_names$new_file_inp)
-
-rm(net_input_01)
+network_results <- readRDS("./data/network_results.rds")
 
 #...............................................................................
-# 3. Running a Full Simulation                                             ####
-#    The function ENepanet() runs a full simulation and 
-#    writes the results to a file. 
+# 4. Create the Fault Signature Matrix/Vector (FSM)
 #...............................................................................
 
-ENepanet(params$f_names$base_file_inp, 
-         params$f_names$base_file_report)
+sensor_pressure <- network_results$sensor_pressure[[2]]
+sensor_flow     <- network_results$sensor_flow[[2]]
 
-ENepanet(params$f_names$new_file_inp,
-         params$f_names$new_file_report)
+for(i in 3:length(network_results$id_result)) {
+  
+  sensor_pressure <- rbind( sensor_pressure,
+                            network_results$sensor_pressure[[i]])
+  
+  sensor_flow     <- rbind( sensor_flow,
+                            network_results$sensor_flow[[i]])
+  
+}
 
-net_input_01  <- read.inp(params$f_names$new_file_inp)
-report_base   <- read.rpt(params$f_names$base_file_report)
-report_leack  <- read.rpt(params$f_names$new_file_report)
+sensor_pressure$sensor_type <- "Pressure"
+sensor_flow$sensor_type     <- "flow"
+
+sensors_PF <- rbind(sensor_pressure, sensor_flow)
 
 #...............................................................................
-# 4. pattern to estimate the evolution of the consumption                   ####
+remove(i, sensor_pressure, sensor_flow, network_results)
 #...............................................................................
 
-# New Leaks (EMITTERS)
+sensor_residual     <- sensors_PF %>%
+                       select(timeInSeconds, sensor_type, ID, ID_leak, Residual) %>%
+                       group_by(timeInSeconds, sensor_type) %>%
+                       nest() %>%
+                       rename(Residual = data)
 
-emitters <- as.tibble(net_input_01$Emitters)
+sensor_Sensitivity  <- sensors_PF %>%
+                       select(timeInSeconds, sensor_type, ID, ID_leak, Sensitivity) %>%
+                       group_by(timeInSeconds, sensor_type) %>%
+                       nest() %>%
+                       rename(Sensitivity = data)
 
-# 4.1.- the average inflow f(t) was calculated at each hour t
-
-base_inletflow <- inlet_flows(report_base,  params$inlet_valves, group = FALSE) 
-leak_inletflow <- inlet_flows(report_leack, params$inlet_valves, group = FALSE)
-
-inletflow      <- full_join(base_inletflow, leak_inletflow, 
-                            by = "timeInSeconds") %>%
-                  select(timeInSeconds, inflow.x, inflow.y)  %>%
-                  mutate(leakflow = inflow.y - inflow.x)
-
-
-rm(base_inletflow,leak_inletflow)
-
-# 4.2.- the average network pressure p(t) was calculated at each hour t
-
-rep01 <- eval_nodes (report_base, node_type = "",
-                     id_nodes  = params$jt_to_analyze, 
-                     group = FALSE) %>%
-                     group_by(timeInSeconds) %>%
-                     summarise(p_median = median(Pressure))
-                
-rep02 <- eval_nodes (report_leack, node_type = "",
-                     id_nodes  = params$jt_to_analyze, 
-                     group = FALSE) %>%
-                     group_by(timeInSeconds) %>%
-                     summarise(p_median = median(Pressure))
-
-rep_p_time <-  full_join(rep01, rep02, by = "timeInSeconds" )
-
-global_emitter <-  full_join(rep_p_time,inletflow, by = "timeInSeconds" )
-
-rm(rep01,rep02, rep_p_time)
-
-# calculate a global emitter
-
-global_emitter <- global_emitter %>%
-                  mutate(DMA_FlowCoef = leakflow/sqrt(p_median.y))
-
-# residual vector
-
-rep01 <- eval_nodes (report_base, node_type = "",
-                     id_nodes  = params$jt_to_analyze, 
-                     group = FALSE)
-
-rep02 <- eval_nodes (report_leack, node_type = "",
-                     id_nodes  = params$jt_to_analyze, 
-                     group = FALSE)
-
-residual <- full_join(rep01, rep02, by = c("timeInSeconds","ID")) %>%
-            mutate(D_Pressure = Pressure.y - Pressure.x)
-
-# sensitivity vector
-emitters
-summary(inletflow)
-
-# detection capability matrix Mdc Where : 
-# - the rows represent the pontential sensor locations and 
-# - the columns represent the potential leak location
+sensors_PF <- full_join(sensor_residual, sensor_Sensitivity, 
+                        by = c("timeInSeconds","sensor_type"))
 
 
+remove(sensor_residual, sensor_Sensitivity)
+#...............................................................................
 
+# glimpse(sensors_PF)
+
+# test_01 <- spread( sensors_PF$Sensitivity[[1]], ID_leak, Sensitivity)
 
